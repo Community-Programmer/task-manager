ebe3fd5a2c2204c055679f7878be9e6dc4988876
Community-Programmer <sarthakpatel230204@gmail.com>
2025-09-02 14:19:49 +0530

    Add global error handler and update dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { config } from "dotenv";
 import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 config();
 
@@ -9,9 +10,12 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Task Manager - API");
-}); 
+});
+
+
+app.use(globalErrorHandler);
 
 
 export default app;
